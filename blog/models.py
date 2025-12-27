1e8665ef3b63d63788eaa2f71f4f24df2affd60e
SalsabilaHanaPradipta <a710230057@student.ums.ac.id>
2025-12-27 11:18:29 +0700

    Menyelesaikan Tugas Bab 9 - Komentar dan Auth

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,8 @@
-from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -15,3 +14,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
